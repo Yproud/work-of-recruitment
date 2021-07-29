5676282700e63e3e6fd672173348594bd8980013
Proudy <eternity.shame@gmail.com>
2021-07-29 14:21:30 +0800

    主运行代码

---
+++ employee/src/main/java/com/xuexi/manager/employee/EmployeeApplication.java
@@ -0,0 +1,19 @@
+package com.xuexi.manager.employee;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan(basePackages = {"com.xuexi.manager.employee.dao"})
+public class EmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApplication.class, args);
+    }
+
+}
