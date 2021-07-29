e36987feb6b31189f8d6c75f5e9a31110fee388e
Proudy <eternity.shame@gmail.com>
2021-07-29 15:00:11 +0800

    主运行代码

---
+++ recruitment-third-party/src/main/java/com/xuexi/manager/thirdparty/GulimallThirdPartyApplication.java
@@ -0,0 +1,18 @@
+package com.xuexi.manager.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan("com.xuexi.manager")
+public class GulimallThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThirdPartyApplication.class, args);
+    }
+
+}
