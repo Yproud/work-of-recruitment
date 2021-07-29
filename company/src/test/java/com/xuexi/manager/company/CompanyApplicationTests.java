20a0161035f4dbc5f70fc56fb0a581b112974398
Proudy <eternity.shame@gmail.com>
2021-07-29 13:34:16 +0800

    运行主代码

---
+++ company/src/test/java/com/xuexi/manager/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuexi.manager.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
